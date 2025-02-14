a55fd0554cfc975b6a60dee104294b81d87c9556
Joe Callahan <joecallahan@macbookair.lan>
2025-02-14 11:56:57 -0800

    pushing up completed workshop

---
+++ employees.js
@@ -0,0 +1,24 @@
+const employees = [
+  {
+    id: 1,
+    name: 'Michael',
+    position: 'Regional Manager'
+  },
+  {
+    id: 2,
+    name: 'Jim',
+    position: 'Sales'
+  },
+  {
+    id: 3,
+    name: 'Dwight',
+    position: 'Assistant to the Regional Manager'
+  },
+  {
+    id: 4,
+    name: 'Pam',
+    position: 'Receptionist'
+  }
+]
+
+module.exports = employees;
---
+++ server.js
@@ -0,0 +1,27 @@
+const employees = require('./employees.js');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`Hello employees!`);
+});
+
+app.get('/employees', (req, res) => {
+  res.send(employees);
+});
+
+app.get('/employees/:id', (req, res) => {
+  const { id } = req.params;
+  const foundEmployee = employees.find((individualEmployee) => {
+    if(id === `random`) {
+      return individualEmployee.id === Math.floor(1+(Math.random() * employees.length));
+    } else {
+      return individualEmployee.id === Number(id);
+    }
+  });
+  res.send(foundEmployee);
+});
+
+app.listen(3000, () => {
+  console.log(`listening on port 3000`);
+});
